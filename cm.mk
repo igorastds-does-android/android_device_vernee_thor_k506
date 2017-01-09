c629f2e534ae172f028134037145b606d9e0878f
Gaboros <gaboros92@gmail.com>
2017-01-09 19:02:14 +0100

    AOSP and TWRP build

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := thor_k506
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/vernee/thor_k506/device_thor_k506.mk)
+$(call inherit-product, device/vernee/thor_k506/full_thor_k506.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := thor_k506
