44ac2f707c0a12af038f4a4911e8eca47f9a7aff
Gaboros <gaboros92@gmail.com>
2017-01-15 13:23:30 +0100

    cannot locate symbol "__xlog_buf_printf" fix

---
+++ apply_patches.sh
@@ -0,0 +1,3 @@
+cd ~/android/lineage14.1/system/core
+git apply -v ~/android/lineage14.1/device/vernee/thor_k506/patches/system_core_liblog.patch
+cd ~/android/lineage14.1/device/vernee/thor_k506
--- cm.mk
+++ cm.mk
@@ -4,6 +4,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/vernee/thor_k506/full_thor_k506.mk)
 
+BOARD_USES_MTK_HARDWARE := true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := thor_k506
 PRODUCT_NAME := cm_thor_k506
