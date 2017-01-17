50806aed1f1b6510e0bad512c31d56b5b9d11dc7
Gaboros <gaboros92@gmail.com>
2017-01-17 17:37:28 +0100

    Warning cleanups

--- device.mk
+++ device.mk
@@ -194,12 +194,6 @@ PRODUCT_PACKAGES += \
 	dhcpcd.conf \
     wpa_supplicant
 	
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/WMT_SOC.cfg:system/etc/firmware/WMT_SOC.cfg \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
-	
 # Configs
 PRODUCT_COPY_FILES += \
     vendor/vernee/thor_k506/prebuilt/etc/ecc_list.xml:system/etc/ecc_list.xml \
